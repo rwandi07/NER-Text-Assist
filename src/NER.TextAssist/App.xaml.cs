62b261c36013aa0b2f53046a3d3260c0d30d4a01
rwandi07 <riswandihelwie@gmail.com>
2026-09-06 12:29:12 +0700

    feat: initialize persistent app data directories on startup

--- src/NER.TextAssist/App.xaml.cs
+++ src/NER.TextAssist/App.xaml.cs
@@ -1,7 +1,13 @@
 using System.Windows;
+using NER.TextAssist.Core;
 
 namespace NER.TextAssist;
 
 public partial class App : Application
 {
+    protected override void OnStartup(StartupEventArgs e)
+    {
+        AppPaths.EnsureCreated();
+        base.OnStartup(e);
+    }
 }
